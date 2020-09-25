6035a28307e2eda8489268ec545cbf2382fb8556
Tiago Oliveira <tiago.marques.oliveira@cern.ch>
2020-09-25 11:14:14 +0200

    Update daq parent

--- src/main/java/cern/c2mon/daq/common/ConfigurationInitializer.java
+++ src/main/java/cern/c2mon/daq/common/ConfigurationInitializer.java
@@ -80,7 +80,7 @@ public class ConfigurationInitializer {
 
     // Causes an exception on the server from 1.9.3-SNAPSHOT onwards (RangeAlarmCondition is the new class)
     configurationService.createAlarm(metrics.getCpuTemperature(),
-        Alarm.create("/cpu.temp", "high", 1, new RangeAlarmCondition<Integer>(0, 90))
+        Alarm.create("/cpu.temp", "high", 1, new RangeAlarmCondition(0, 90, true))
             .addMetadata("causes", "The CPU temperature is too high")
             .build());
 
